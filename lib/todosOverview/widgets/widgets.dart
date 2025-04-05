8b46880c0470646b5513199edf12f37dd1f6e253
C12VS01vedant <vedantsagolale12@gmail.com>
2025-04-05 22:33:31 +0530

    todos-bloc

---
+++ lib/todosOverview/widgets/widgets.dart
@@ -0,0 +1,3 @@
+export 'todos_list_tile.dart';
+export 'todos_overview_filter_button.dart';
+export 'todos_overview_options_button.dart';
---
+++ test/stats/bloc/stats_event_test.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:todos/stats/bloc/stats_bloc.dart';
+
+void main() {
+  group('StatsEvent', () {
+    group('StatsSubscriptionRequested', () {
+      test('supports value equality', () {
+        expect(
+          StatsSubscriptionRequested(),
+          equals(StatsSubscriptionRequested()),
+        );
+      });
+
+      test('props are correct', () {
+        expect(StatsSubscriptionRequested().props, equals(<Object?>[]));
+      });
+    });
+  });
+}
